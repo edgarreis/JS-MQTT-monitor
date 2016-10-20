ef127824d56c4fcfa740552e073e9698ce1923f9
edgarreis <edgar_dreis@hotmail.com>
2016-10-20 16:05:35 -0200

    Changed
    Changed topics and variables, Add buttons and configs

--- conf.js
+++ conf.js
@@ -1,6 +1,8 @@
-var websocketserver = 'broker.hivemq.com'; //'192.168.25.4';
-var websocketport   = 8000;
-var topic = 'clients/arduino/#';
 
-var ledtopic = 'clients/arduino/led'	// Web PUBlishes to ledtopic + "/cmd"
+// Script JavaScript
+// Config Broker Connection
+var websocketserver = 'iot.eclipse.org'; //'broker.hivemq.com'; //'192.168.25.4'; //'192.168.110.82';
+var websocketport   =  80; 
+var topic = 'dev/node1/#';
 
+var ledtopic = 'clients/arduino/led';	// Web PUBlishes to ledtopic + "/cmd"
